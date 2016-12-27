76aba6c26f0933e3822d1ab1d4ef01562d02a884
Eike <eike@medienpaedagogik-praxis.de>
2016-12-27 17:45:27 +0100

    Formular integriert, TNmax auf 50 erhöht

--- js/script.js
+++ js/script.js
@@ -4,30 +4,35 @@ var intervall = 500;
 var summe = 0;
 var kosten = (satz / (3600 * (1000 / intervall)));
 var counter = 0;
+var anwesende = 1;
 
 $(document).ready(function() {
     $("#anzeige").text(summe.toFixed(2)+" CHF");
     $("#start").click(function() {
-        $(this).addClass("disabled");
-        $(this).removeClass("active");
-        $("#stop").addClass("active");
-        $("#stop").removeClass("disabled");
-        $("#reset").addClass("disabled");
-        $("#reset").removeClass("active");
-        counter = setInterval(function(){summe = summe + kosten; $("#anzeige").text(summe.toFixed(2)+" CHF");},intervall);
+      anwesende = document.getElementById("anwesende").value;;
+      $(this).addClass("disabled");
+      $(this).removeClass("active");
+      $("#stop").addClass("active");
+      $("#stop").removeClass("disabled");
+      $("#reset").addClass("disabled");
+      $("#reset").removeClass("active");
+      counter = setInterval(function(){
+        summe = summe + (kosten * anwesende);
+        $("#anzeige").text(summe.toFixed(2)+" CHF");
+      },intervall);
     });
     $("#stop").click(function(){
-        $(this).addClass("disabled");
-        $(this).removeClass("active");
-        $("#start").addClass("active");
-        $("#start").removeClass("disabled");
-        $("#reset").addClass("active");
-        $("#reset").removeClass("disabled");
-        clearInterval(counter);
+      $(this).addClass("disabled");
+      $(this).removeClass("active");
+      $("#start").addClass("active");
+      $("#start").removeClass("disabled");
+      $("#reset").addClass("active");
+      $("#reset").removeClass("disabled");
+      clearInterval(counter);
     });
     $("#reset").click(function(){
       $(this).animate({opacity: '0.5'},"fast");
-      $(this).animate({opacity: '1.0'},"fast");  
+      $(this).animate({opacity: '1.0'},"fast");
       summe = 0;
       $("#anzeige").text(summe.toFixed(2)+" CHF");
     });
